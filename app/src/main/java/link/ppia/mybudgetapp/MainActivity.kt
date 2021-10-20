815307707f23224991763caed950b37d499162f3
Admin <ppia4382@gmail.com>
2021-10-21 02:59:01 +0900

    This is the first commit for MyBudgetApp.

---
+++ app/src/main/java/link/ppia/mybudgetapp/MainActivity.kt
@@ -0,0 +1,73 @@
+package link.ppia.mybudgetapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.Room
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var transactions: List<Transaction>
+    private lateinit var transactionAdapter:TransactionAdapter
+    private lateinit var linearLayoutManager: LinearLayoutManager
+    private lateinit var db: AppDatabase
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        transactions = arrayListOf()
+
+        transactionAdapter = TransactionAdapter(transactions)
+        linearLayoutManager = LinearLayoutManager(this)
+
+        db= Room.databaseBuilder(this,
+        AppDatabase::class.java,
+            "transactions").build()
+
+        recyclerView.apply {
+            adapter = transactionAdapter
+            layoutManager = linearLayoutManager
+
+        }
+
+        addBtn.setOnClickListener {
+            val intent = Intent(this,AddTransactionActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    private fun fetchAll(){
+        GlobalScope.launch {
+            transactions = db.transactionDao().getAll()
+
+            runOnUiThread {
+                updateDashboard()
+                transactionAdapter.setData(transactions)
+            }
+        }
+    }
+
+    private fun updateDashboard(){
+        val totalAmount = transactions.map { it.amount }.sum()
+        val budgetAmount = transactions.filter { it.amount>0 }.map { it.amount }.sum()
+        val expenseAmount = totalAmount - budgetAmount
+
+        balance.text = "¥%.2f".format(totalAmount)
+        budget.text = "¥%.2f".format(budgetAmount)
+        expense.text = "¥%.2f".format(expenseAmount)
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        fetchAll()
+    }
+
+}
